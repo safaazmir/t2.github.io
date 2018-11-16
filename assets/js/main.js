92142b1dea6b21f61f282205a2c16617710138a8
kamrulislamcmt <44641354+kamrulislamcmt@users.noreply.github.com>
2018-11-16 12:26:23 +0600

    t2

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	$(".testimonial-content").owlCarousel({
+		items: 1,
+		nav: false, 
+		dots: true,
+		loop: true,
+		autoplay:true,
+		autoplaySpeed:1500
+	});
+});
